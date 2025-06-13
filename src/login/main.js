8d7cb349a4620a7ac26544efda402530c7c3dfab
Karol-zoned <kargul.karolina47@gmail.com>
2025-06-13 23:14:47 +0200

    first commit

---
+++ src/login/main.js
@@ -0,0 +1,27 @@
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient('https://gdfpwixfrujkoxlejcgo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkZnB3aXhmcnVqa294bGVqY2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTY0NjMsImV4cCI6MjA2MzMzMjQ2M30.btatLN5ciqWbERzsaDvk6yyTI2eJJY1wjTUDFsmA00Q')
+
+const form = document.getElementById('login-form');
+const errorEl = document.getElementById('login-error');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const email = form.email.value;
+  const password = form.password.value;
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    errorEl.textContent = error.message;
+    errorEl.classList.remove('hidden');
+  } else {
+    // np. przekierowanie po zalogowaniu
+    window.location.href = '/lab13/index.html';
+  }
+});
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import fg from 'fast-glob'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const inputs = await fg('src/**/*.html', { absolute: true })
+
+export default defineConfig({
+  base: '/strona/',
+  plugins: [
+    tailwindcss(),
+  ],
+  root: resolve(__dirname, 'src'),
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: inputs,
+    },
+    outDir: resolve(__dirname, 'dist'),
+  },
+})
